62629762a6187dbf2dd15589ddfefe9e59451e0b
rod233 <317426840@qq.com>
2022-05-06 17:33:43 +0800

    6 commit

--- model.py
+++ model.py
@@ -5,9 +5,14 @@ import torch.nn.functional as F
 class MnistModel(nn.Module):
     def __init__(self):
         super(MnistModel,self).__init__()
-        self.fc1 = nn.Linear(in_features=28*28,out_features=28)
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=12, kernel_size=9, stride=2, padding=0),# output shape(12, 10, 10)
+			nn.MaxPool2d(kernel_size=5), #output shape(12, 2, 2)
+			nn.Flatten(),
+			nn.ReLU()
+			)
         # 输出是10个数字
-        self.fc2 = nn.Linear(28,10)
+        self.out = nn.Linear(12 * 2 * 2, 10)
 
 
     def forward(self,input):
@@ -15,11 +20,7 @@ class MnistModel(nn.Module):
         :param input: [batch_size,1,28*28] 我们获得的原始数据的样子，即input[0]=batchsize,input[1]=1,input[2]=28*28
         :return: 
         '''
-        x = input.view(input.size(0),28*28) 
-        #进行全连接操作
-        x = self.fc1(x)
-        #使用激活函数处理数据，不会使形状发生变化
-        x = F.relu(x)
-        #输出层
-        out = self.fc2(x)
+        x = self.conv1(input)       
+		
+        out = self.out(x)
         return out
